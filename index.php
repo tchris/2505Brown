8bed165c7cae0d4f4061f8b7fc3a91e8b1ec4c31
John Szpyrka <john.l.szpyrka@outlook.com>
2025-06-12 16:02:33 -0600

    Changed Title so it is not so obvious I stole this from my other nerd page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Ideal Robot – Creative Profile</title>
+    <title>TRON Cycles – Bike Shop of the Future</title>
     <link rel="stylesheet" href="static/styles.css">
 </head>
 <body>   
