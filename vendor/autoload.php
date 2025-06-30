4811e436b498f0b6bbd881cf49c4f93198ffa524
John Szpyrka <john.l.szpyrka@outlook.com>
2025-06-30 11:28:02 -0600

    Reverting to past pages to see if it resolved commit issue with Github...

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitac2d307f4ed514763a8119f3523066bb::getLoader();
+return ComposerAutoloaderInit00da199c24f69d4e39903c3676bfe248::getLoader();
