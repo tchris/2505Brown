84d9d6d6989bdfe91d343293c8e3b445598ec2f3
John Szpyrka <john.l.szpyrka@outlook.com>
2025-07-07 08:18:24 -0600

    Updated the path.

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $accessories = $mysqli->query("SELECT * FROM Mountain_Bike WHERE Category = 'Acc
         <!-- Hero Wrapper -->
         <div class="hero">
             <img src="img/CP-Bike.png" class="hero__image">
-            <h2 class="cart__menu"><a href="/2505Chartreuse/cart.php"><img src="static/img/tron_cart.png" alt="Cart" class="cart__icon"></a></h2>
+            <h2 class="cart__menu"><a href="/2505Chartreuse/cart.php"><img src="img/tron_cart.png" alt="Cart" class="cart__icon"></a></h2>
             <h1 class="hero__title"><a href="https://alittlespace.org/2505Chartreuse/">TRON Bike Shop</a></h1>
             <h2 class="hero__menu"><a href="/2505Chartreuse/hardtail.php">Hardtail</a> |<a href="/2505Chartreuse/fullsuspension.php"> Full Suspension</a> | <a href="/2505Chartreuse/accessories.php">Accessories Galore</a></h2>
             <h2 class="hero__menu mobile-menu"><a href="/2505Chartreuse/hardtail.php">Hardtail</a><br><a href="/2505Chartreuse/fullsuspension.php">Full Suspension</a><br><a href="/2505Chartreuse/accessories.php">Accessories Galore</a></h2>
