65262ea23157ff511e0bc7a998623077d7323966
John Szpyrka <john.l.szpyrka@outlook.com>
2025-07-07 10:10:46 -0600

    Added inline font change for foot copyright information so it looks cleaner and more subtle.

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
         
             <footer>
                 <h2 class="hero__menu"><a href="/2505Chartreuse/login.php">Managers</a> |<a href="/2505Chartreuse/warehouse_login.php"> Warehouse Managers</a>
-                <p>© 2025 Bikes R' Us. All rights reserved.</p>
+                <p style="font-size: 1.25rem;">© 2025 Bikes R' Us. All rights reserved.</p>
                 <h2 class="hero__menu mobile-menu"><a href="/2505Chartreuse/login.php">Managers</a><br><a href="/2505Chartreuse/fullsuspension.php">Warehouse Managers</a><br>
-                <p>© 2025 Bikes R' Us. All rights reserved.</p>
+                <p style="font-size: 1.25rem;">© 2025 Bikes R' Us. All rights reserved.</p>
             </footer>
