fd8c4a0929aef70c0f8f2d745510386483bf5980
John Szpyrka <john.l.szpyrka@outlook.com>
2025-07-07 10:04:33 -0600

    Updated Manager Links in Footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,7 @@
 <!-- Footer -->
         
             <footer>
+                <h2 class="hero__menu"><a href="/2505Chartreuse/login.php">Managers</a> |<a href="/2505Chartreuse/warehouse_login.php"> Warehouse Managers</a>
+                <h2 class="hero__menu mobile-menu"><a href="/2505Chartreuse/login.php">Managers</a><br><a href="/2505Chartreuse/fullsuspension.php">Warehouse Managers</a><br>
                 <p>© 2025 Bikes R' Us. All rights reserved.</p>
             </footer>
