81e3dcd1a9fac7eb90fdd477d0ede951d5337b0c
John Szpyrka <john.l.szpyrka@outlook.com>
2025-07-07 10:20:11 -0600

    Changed Warehouse Managers in the footer to Fulfillment Team

--- templates/footer.php
+++ templates/footer.php
@@ -2,13 +2,13 @@
 <footer class="footer">
   <h2 class="hero__menu">
     <a href="/2505Chartreuse/login.php">Managers</a> |
-    <a href="/2505Chartreuse/warehouse_login.php">Warehouse Managers</a>
+    <a href="/2505Chartreuse/warehouse_login.php">Fulfillment Team</a>
   </h2>
   <p class="copyright">© 2025 Bikes R' Us. All rights reserved.</p>
 
   <h2 class="hero__menu mobile-menu">
     <a href="/2505Chartreuse/login.php">Managers</a><br>
-    <a href="/2505Chartreuse/warehouse_login.php">Warehouse Managers</a>
+    <a href="/2505Chartreuse/warehouse_login.php">Fulfillment Team</a>
   </h2>
   <p class="copyright mobile-only">© 2025 Bikes R' Us. All rights reserved.</p>
 </footer>
