519d015637e0526be004b567476bb7536bb236b4
John Szpyrka <john.l.szpyrka@outlook.com>
2025-07-07 10:18:57 -0600

    Adjust font weight and size for footer copyright information... updated .css for footer objects.

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,14 @@
 <!-- Footer -->
-        
-            <footer>
-                <h2 class="hero__menu"><a href="/2505Chartreuse/login.php">Managers</a> |<a href="/2505Chartreuse/warehouse_login.php"> Warehouse Managers</a>
-                <p style="font-size: 1.25rem;">© 2025 Bikes R' Us. All rights reserved.</p>
-                <h2 class="hero__menu mobile-menu"><a href="/2505Chartreuse/login.php">Managers</a><br><a href="/2505Chartreuse/fullsuspension.php">Warehouse Managers</a><br>
-                <p style="font-size: 1.25rem;">© 2025 Bikes R' Us. All rights reserved.</p>
-            </footer>
+<footer class="footer">
+  <h2 class="hero__menu">
+    <a href="/2505Chartreuse/login.php">Managers</a> |
+    <a href="/2505Chartreuse/warehouse_login.php">Warehouse Managers</a>
+  </h2>
+  <p class="copyright">© 2025 Bikes R' Us. All rights reserved.</p>
+
+  <h2 class="hero__menu mobile-menu">
+    <a href="/2505Chartreuse/login.php">Managers</a><br>
+    <a href="/2505Chartreuse/warehouse_login.php">Warehouse Managers</a>
+  </h2>
+  <p class="copyright mobile-only">© 2025 Bikes R' Us. All rights reserved.</p>
+</footer>
